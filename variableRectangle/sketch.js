b6e29d49a768b4dd8804393b39025ad807c79992
Eric Vargas <eric.vargas719@gmail.com>
2017-02-24 15:56:06 -0500

    Update sketch.js

--- variableRectangle/sketch.js
+++ variableRectangle/sketch.js
@@ -1,29 +1,26 @@
-var rectX = 27.5;
+var rectX = 27.5;                                   // declaring values for future refernece
 var w = 750 - 27.5;
 
-function setup() {
+function setup() {                                  //creating work area
   createCanvas(750, 375);
 }
 
 function draw() {
   // background
   background(255);
-  rectX = rectX + 8;
+  rectX = rectX + 8;                               //moving shape, increments by 8
   if (rectX > w) {
-    rectX = 27.5; //w - 20
+    rectX = 27.5;                                  //w - 20 (bounce effect)
   }
-  // rectangle
-  noStroke();
+  noStroke();                                      // rectangle
   fill(mouseY, 300, 0);
   rect(rectX, mouseY, 55, 55);
 }
 
-function mouseReleased() {
+function mouseReleased() {                        // shape loops as long as mouse not clicked
   loop();
 }
   
-  function mousePressed() {
+  function mousePressed() {                       // loop stops when mouse is clicked and held
     noLoop();
   }
-  // line(mouseX, mouseY, pmouseX, pmouseY);
- //  print(pmouseX + "->" + mouseX);
